887c6422e81ac0c6975e8cfba7ec3aaa86467103
DaoTrongHieu06072000 <tronghieu06072000@gmail.com>
2020-11-24 14:33:52 +0700

    Tạo hàm count

--- src/main/java/com/btl/nmcnpm/dao/impl/AbstractDAO.java
+++ src/main/java/com/btl/nmcnpm/dao/impl/AbstractDAO.java
@@ -159,6 +159,35 @@ public class AbstractDAO<T> implements GenericDAO<T> {
 	}
 	@Override
 	public int count(String sql, Object... parameters) {
-		return 0;
+		Connection connection = null;
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
+		try {
+			int count = 0;
+			connection = getConnection();
+			statement = connection.prepareStatement(sql);
+			setParameter(statement, parameters);
+			resultSet = statement.executeQuery();
+			while (resultSet.next()) {
+				count = resultSet.getInt(1);
+			}
+			return count;
+		} catch (SQLException e) {
+			return 0;
+		} finally {
+			try {
+				if (connection != null) {
+					connection.close();
+				}
+				if (statement != null) {
+					statement.close();
+				}
+				if (resultSet != null) {
+					resultSet.close();
+				}
+			} catch (SQLException e) {
+				return 0;
+			}
+		}
 	}
 }
