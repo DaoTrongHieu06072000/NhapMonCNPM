51b72a1d48936c32b020c361638a7452eb994f3e
DaoTrongHieu06072000 <tronghieu06072000@gmail.com>
2020-11-25 21:01:44 +0700

    Tạo file chuyển LoaiDongGop từ resultSet sang Mode

--- src/main/java/com/btl/nmcnpm/mappers/LoaiDongGopMapper.java
+++ src/main/java/com/btl/nmcnpm/mappers/LoaiDongGopMapper.java
@@ -1,4 +1,23 @@
 package com.btl.nmcnpm.mappers;
 
-public class LoaiDongGopMapper {
+import com.btl.nmcnpm.models.LoaiDongGopModel;
+
+import java.sql.ResultSet;
+
+public class LoaiDongGopMapper implements com.btl.nmcnpm.mapper.RowMapper {
+
+    @Override
+    public LoaiDongGopModel mapRow(ResultSet rs) {
+        try {
+
+            LoaiDongGopModel loaiDongGop = new LoaiDongGopModel();
+            loaiDongGop.setCode(rs.getString("code"));
+            loaiDongGop.setIdLoaiDongGop(rs.getInt("idLoaiDongGop"));
+            loaiDongGop.setName(rs.getString("name"));
+            return loaiDongGop;
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
