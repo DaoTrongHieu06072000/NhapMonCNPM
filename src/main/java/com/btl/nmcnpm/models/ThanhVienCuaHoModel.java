a947166fcda801b3d47e88eaddbec2044187e620
ngovanquang <banbanhbeo@outlook.com>
2020-11-25 18:23:55 +0700

    tạo các Model

---
+++ src/main/java/com/btl/nmcnpm/models/ThanhVienCuaHoModel.java
@@ -0,0 +1,31 @@
+package com.btl.nmcnpm.models;
+
+public class ThanhVienCuaHoModel {
+    private Integer idNhanKhau;
+    private Integer idHoKhau;
+    private String quanHeVoiChuHo;
+
+    public Integer getIdNhanKhau() {
+        return idNhanKhau;
+    }
+
+    public void setIdNhanKhau(Integer idNhanKhau) {
+        this.idNhanKhau = idNhanKhau;
+    }
+
+    public Integer getIdHoKhau() {
+        return idHoKhau;
+    }
+
+    public void setIdHoKhau(Integer idHoKhau) {
+        this.idHoKhau = idHoKhau;
+    }
+
+    public String getQuanHeVoiChuHo() {
+        return quanHeVoiChuHo;
+    }
+
+    public void setQuanHeVoiChuHo(String quanHeVoiChuHo) {
+        this.quanHeVoiChuHo = quanHeVoiChuHo;
+    }
+}
---
+++ src/main/java/com/btl/nmcnpm/models/UserModel.java
@@ -0,0 +1,31 @@
+package com.btl.nmcnpm.models;
+
+public class UserModel {
+    private Integer ID;
+    private String userName;
+    private String passwd;
+
+    public Integer getID() {
+        return ID;
+    }
+
+    public void setID(Integer ID) {
+        this.ID = ID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+}
