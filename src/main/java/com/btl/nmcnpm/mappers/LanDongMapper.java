a2d0dd07615dae5f1fcd968c05a2e0009dd7f94f
DaoTrongHieu06072000 <tronghieu06072000@gmail.com>
2020-11-25 21:07:04 +0700

    Tạo file LanDongMapper

--- src/main/java/com/btl/nmcnpm/mappers/LanDongMapper.java
+++ src/main/java/com/btl/nmcnpm/mappers/LanDongMapper.java
@@ -1,5 +1,24 @@
 package com.btl.nmcnpm.mappers;
 
-public class LanDongMapper {
+import com.btl.nmcnpm.mapper.RowMapper;
+import com.btl.nmcnpm.models.LanDongModel;
+import java.sql.ResultSet;
+
+public class LanDongMapper implements RowMapper {
+
+    @Override
+    public Object mapRow(ResultSet rs) {
+        try {
+            LanDongModel lanDong = new LanDongModel();
+            lanDong.setIdLanDong(rs.getInt("idLanDong"));
+            lanDong.setLanThu(rs.getInt("lanThu"));
+            lanDong.setNgay(rs.getTimestamp("ngay"));
+            return lanDong;
+        } catch (Exception e) {
+            e.printStackTrace();;
+            return null;
+        }
+
+    }
 
 }
