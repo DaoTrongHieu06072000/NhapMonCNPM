ee98774a4d683934c4257629aaab8b3b8c6987d0
SonTH183823 <sonto2k@gmail.com>
2020-11-26 15:53:34 +0700

    tạo form tạo một bản thu phí vệ sinh mới

--- src/views/PhiVS.java
+++ src/views/PhiVS.java
@@ -5,6 +5,9 @@
  */
 package views;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.util.Locale;
 import javax.swing.JFrame;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
@@ -27,22 +30,23 @@ public class PhiVS extends javax.swing.JPanel {
         initComponents();
         JFreeChart pieChart = createChart(createDataset());
         ChartPanel chartPanel = new ChartPanel(pieChart);
-        GraphjPanel1.add(chartPanel);
+        GraphjPanel1.removeAll();
+        GraphjPanel1.add(chartPanel, BorderLayout.CENTER);
+        GraphjPanel1.validate();
         GraphjPanel1.setSize(600, 400);
-        GraphjPanel1.setVisible(true);
+//        GraphjPanel1.setBackground(Color.white);
     }
 
     private static JFreeChart createChart(PieDataset dataset) {
-        JFreeChart chart = ChartFactory.createPieChart(
-                "CƠ CẤU DÂN SỐ THEO NHÓM TUỔI NĂM 2020", dataset, true, true, true);
+        JFreeChart chart = ChartFactory.createPieChart("", dataset, false, false, Locale.ITALY);
         return chart;
     }
 
     private static PieDataset createDataset() {
         DefaultPieDataset dataset = new DefaultPieDataset();
-        dataset.setValue("Nhóm 0 - 14", new Double(50.0));
-        dataset.setValue("Nhóm 15 - 59", new Double(41.0));
-        dataset.setValue("Nhóm trên 60", new Double(9.0));
+        dataset.setValue("Đã đóng 67%", new Double(67.0));
+        dataset.setValue("Chưa đóng 33%", new Double(33.0));
+        //dataset.setValue("dsafs", new Double(3.2));
         return dataset;
     }
     
@@ -51,37 +55,58 @@ public class PhiVS extends javax.swing.JPanel {
     private void initComponents() {
 
         GraphjPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jComboBox1 = new javax.swing.JComboBox<>();
+        jLabel2 = new javax.swing.JLabel();
 
         setBackground(new java.awt.Color(255, 204, 102));
         setBorder(javax.swing.BorderFactory.createTitledBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 1, true), "Quản Lý Phí Vệ Sinh", javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.TOP, new java.awt.Font("Tahoma", 1, 18))); // NOI18N
         setPreferredSize(new java.awt.Dimension(703, 542));
 
-        javax.swing.GroupLayout GraphjPanel1Layout = new javax.swing.GroupLayout(GraphjPanel1);
-        GraphjPanel1.setLayout(GraphjPanel1Layout);
-        GraphjPanel1Layout.setHorizontalGroup(
-            GraphjPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 326, Short.MAX_VALUE)
-        );
-        GraphjPanel1Layout.setVerticalGroup(
-            GraphjPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 264, Short.MAX_VALUE)
-        );
+        GraphjPanel1.setBorder(new javax.swing.border.MatteBorder(null));
+        GraphjPanel1.setPreferredSize(new java.awt.Dimension(600, 400));
+        GraphjPanel1.setLayout(new java.awt.BorderLayout());
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel1.setText("Biểu đồ tỉ lệ các hộ đã đóng");
+
+        jComboBox1.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jComboBox1.setMaximumRowCount(5);
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "2017", "2018", "2019", "2020", "" }));
+        jComboBox1.setSelectedIndex(3);
+        jComboBox1.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 1, true));
+        jComboBox1.setName("Năm"); // NOI18N
+
+        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel2.setText("Năm");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(60, 60, 60)
-                .addComponent(GraphjPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(347, Short.MAX_VALUE))
+                .addGap(49, 49, 49)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(GraphjPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(398, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(80, 80, 80)
-                .addComponent(GraphjPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(136, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(39, 39, 39)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 32, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(GraphjPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 201, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(184, 184, 184))
         );
     }// </editor-fold>//GEN-END:initComponents
 
@@ -91,5 +116,8 @@ public class PhiVS extends javax.swing.JPanel {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel GraphjPanel1;
+    private javax.swing.JComboBox<String> jComboBox1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     // End of variables declaration//GEN-END:variables
 }
