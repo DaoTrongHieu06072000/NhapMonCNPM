3a4b53341ca783ee6a0d477a8aec6169c82edbea
SonTH183823 <sonto2k@gmail.com>
2020-12-02 00:11:06 +0700

    Chỉnh sửa lại dữ liệu đẩy lên trang chủ từ DB

--- src/main/java/com/btl/nhapmoncnpm/dao/IUserDAO.java
+++
@@ -1,9 +0,0 @@
-package com.btl.nhapmoncnpm.dao;
-
-import services.DAO.GenericDAO;
-import com.btl.nhapmoncnpm.models.UserModel;
-import java.util.List;
-
-public interface IUserDAO extends GenericDAO<UserModel> {
-    List<UserModel> findAll();
-}
---
+++ src/main/java/service/DAO/IMPL/NhanKhauDAO.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package services.DAO.IMPL;
+
+import java.util.List;
+import model.NhanKhauModel;
+import services.DAO.INhanKhauDAO;
+import services.Mapper.NhanKhauMapper;
+
+/**
+ *
+ * @author banba
+ */
+public class NhanKhauDAO extends AbstractDAO<NhanKhauModel> implements INhanKhauDAO {
+
+    @Override
+    public List<NhanKhauModel> findAll() {
+        String sql = "SELECT * FROM nhan_khau";
+        return query(sql, new NhanKhauMapper());
+    }
+
+    @Override
+    public NhanKhauModel findById(int id) {
+        String sql = "SELECT * FROM nhan_khau WHERE ID = ?";
+        List<NhanKhauModel> nhanKhauModels = query(sql, new NhanKhauMapper(), id);
+        return nhanKhauModels.isEmpty() ? null : nhanKhauModels.get(0);
+    }
+
+    @Override
+    public  List<NhanKhauModel> findByName(String hoten) {
+        String sql = "SELECT * FORM nhan_khau WHERE hoTen LIKE ?";
+        String bien = "'%" + hoten +"%'";
+        return query(sql, new NhanKhauMapper(), bien);
+        //return listnhanKhauModels;
+    }
+    
+}
